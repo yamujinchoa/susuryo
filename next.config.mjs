4325a56f921e7db6bb62489ae8d139a113915112
yamujinchoa <yamujinchoa@gmail.com>
2024-10-13 00:30:05 +0900

    Update next.config.mjs for App Router support in src directory
    Update next.config.mjs for App Router support in src directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true, // App Router 사용 설정
+  },
+};
 
 export default nextConfig;
