ee831d60d66d858511257b84966e5aa960f836e9
yamujinchoa <yamujinchoa@gmail.com>
2024-10-27 23:21:19 +0900

    supabaseClient 설정 변경으로 로컬 스토리지에 저장하지 않음

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -4,4 +4,9 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    autoRefreshToken: false,
+    persistSession: false,
+  },
+});
