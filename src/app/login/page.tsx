5380ad04565a866e15c8a7688c746027819e1714
yamujinchoa <yamujinchoa@gmail.com>
2024-11-01 00:08:42 +0900

    로그인 로직 변경
    1. 이메일과 비밀번호 입력.
    2. 로그인 버튼을 클릭하면, 캡챠 실행.
    3. 캡챠 인증이 완료되면, 자동으로 로그인 요청이 진행되도록 구현.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,26 +2,40 @@
 "use client";
 
 import HCaptcha from "@hcaptcha/react-hcaptcha";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { login } from "./actions";
 
 export default function LoginPage() {
   const [token, setToken] = useState<string | null>(null);
   const captchaRef = useRef<HCaptcha>(null);
+  const [formData, setFormData] = useState<FormData | null>(null);
 
-  const handleAction = async (formData: FormData) => {
+  useEffect(() => {
+    // token이 설정되면 login 호출
+    if (token && formData) {
+      formData.append("token", token);
+      login(formData);
+    }
+  }, [token, formData]);
+
+  const handleAction = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // 기본 폼 제출 방지
+
+    const newFormData = new FormData(event.currentTarget);
+    setFormData(newFormData); // formData 저장
+
+    // 토큰이 없으면 캡챠 실행
     if (!token) {
       captchaRef.current?.execute();
       return;
     }
-    formData.append("token", token);
   };
 
   return (
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-12 col-sm-8 col-md-6">
-          <form className="mt-4">
+          <form className="mt-4" onSubmit={handleAction}>
             <div className="mb-3">
               <label htmlFor="email" className="form-label">
                 이메일
@@ -53,13 +67,7 @@ export default function LoginPage() {
               size="invisible"
             />
             <div className="d-grid gap-2">
-              <button
-                className="btn btn-primary"
-                formAction={async (formData) => {
-                  await handleAction(formData);
-                  await login(formData);
-                }}
-              >
+              <button type="submit" className="btn btn-primary">
                 로그인
               </button>
             </div>
